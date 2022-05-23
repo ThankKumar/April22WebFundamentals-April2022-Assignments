4cdd2e4ba37fb0eb432fb7c754f93548a7af275b
Mohammad Altaf Bazaz <86340586+altafbazaz7@users.noreply.github.com>
2022-05-24 00:10:28 +0530

    Add files via upload

---
+++ JavascriptProjects/Project3/project3.js
@@ -0,0 +1,31 @@
+(function () {
+  let screen = document.querySelector(".screen");
+  let btn = document.querySelectorAll(".buttons");
+  let clear = document.querySelector(".btn-red");
+  let equal = document.querySelector(".btn-green");
+
+  btn.forEach(function(button){
+      button.addEventListener('click',function(e){
+            let value = e.target.dataset.num;
+            screen.value += value;
+            
+      })
+  })
+
+  equal.addEventListener('click', function(e){
+    if(screen.value === ' '){
+      screen.value = ' '
+    }else{
+      let answer =   eval(screen.value);
+      screen.value = answer;
+    }
+  })
+
+  clear.addEventListener('click' , function(e){
+        screen.value = ''
+  })
+
+
+
+
+})();
